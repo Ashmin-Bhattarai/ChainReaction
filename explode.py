819c75ed6022d6747a1c6b8145bb17527ff28015
Ashmin Bhattarai <koushal.ashmin@gmail.com>
2021-07-21 23:24:39 +0545

    blast animation

--- explode.py
+++ explode.py
@@ -1,6 +1,7 @@
 from winsound import *
 import _thread
 import sys
+import time
 
 
 class Explode:
@@ -332,3 +333,131 @@ class Explode:
         # if len(cord_list) != 0:
         #     print("x,y, v = ", x, y, cells[x][y][0])
         #     explode()
+
+    def adjustCell(self):
+        # print("inside adjustCell")
+        nextGen = []
+        for blist in self.blastList:
+            x, y, n, d = blist[0], blist[1], blist[2], blist[3]
+            tag = "circle_" + str(x) + "_" + str(y) + "_"
+
+            self.cells[x][y][0] = self.cells[x][y][0] - n
+
+            if self.cells[x][y][0] == 0:
+                self.cells[x][y][1] = 0
+            else:
+                self.cells[x][y][1] = self.player
+
+            for i in range(0, n):
+                self.cells[(x + d[i][0])][(y + d[i][1])][0] += 1
+                self.cells[(x + d[i][0])][(y + d[i][1])][1] = self.player
+
+                if [x + d[i][0], y + d[i][1]] not in nextGen:                
+                    nextGen.append([x + d[i][0], y + d[i][1]])
+                    
+                self.c.delete(tag + str(i+1))
+                self.drawCircles(x + d[i][0], y + d[i][1])
+
+            self.drawCircles(x, y)
+        if len(nextGen) != 0:
+            self.cord_list2.append(nextGen)
+
+    def animate(self):
+        self.i = 0
+        self.vel = 5
+        # print("inside animate")
+
+        def moveCircle():
+
+            for blist in self.blastList:
+                x, y, n, d = blist[0], blist[1], blist[2], blist[3]
+                tag = "circle_" + str(x) + "_" + str(y) + "_"
+                # self.play_sound("soundeffects/explode.wav")
+                for i in range(0, n):
+                    # print("Inside animate: ",tag + str(i+1))
+                    self.c.move(tag + str(i+1),
+                                d[i][0] * self.vel, d[i][1] * self.vel)
+                self.c.update()
+
+            self.i += self.vel
+            if self.i >= self.xd:
+                return
+            else:
+                #print("sleep")
+                # time.sleep(0.1)
+                moveCircle()
+        if len(self.blastList) != 0:
+            moveCircle()
+
+    def explode2(self):
+        #print("inside explode2")
+        self.blastList = []  # [x, y, 2 for corners 3 for edge 4 for middle, [direction] ]
+
+        firstGen = self.cord_list2[0]
+
+        for cord in firstGen:
+            x, y = cord[0], cord[1]
+
+            # Upper Left Corner
+            if x == 0 and y == 0 and self.cells[x][y][0] >= 2:
+                # print("satisfy")
+                #self.animate_corner(tag, 1, 0, 0, 1)
+                self.blastList.append([x, y, 2, [[1, 0], [0, 1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # Upper Right Corner
+            elif x == self.size - 1 and y == 0 and self.cells[x][y][0] >= 2:
+                # self.animate_corner(tag, -1, 0, 0, 1)
+                self.blastList.append([x, y, 2, [[-1, 0], [0, 1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # lower left corner
+            elif x == 0 and y == self.size - 1 and self.cells[x][y][0] >= 2:
+                # self.animate_corner(tag, 1, 0, 0, -1)
+                self.blastList.append([x, y, 2, [[1, 0], [0, -1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # lower right corner
+            elif x == self.size - 1 and y == self.size - 1 and self.cells[x][y][0] >= 2:
+                # self.animate_corner(tag, -1, 0, 0, -1)
+                self.blastList.append([x, y, 2, [[-1, 0], [0, -1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # left edge
+            elif x == 0 and y in range(1, self.size - 1) and self.cells[x][y][0] >= 3:
+                # self.animate_edge(tag, 0, 1, 1, 0, 0, -1)
+                self.blastList.append([x, y, 3, [[0, 1], [1, 0], [0, -1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # right edge
+            elif (x == self.size - 1 and y in range(1, self.size - 1) and self.cells[x][y][0] >= 3):
+                # self.animate_edge(tag, -1, 0, 0, 1, 0, -1)
+                self.blastList.append([x, y, 3, [[-1, 0], [0, 1], [0, -1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # upper edge
+            elif y == 0 and x in range(1, self.size - 1) and self.cells[x][y][0] >= 3:
+                # self.animate_edge(tag, -1, 0, 1, 0, 0, 1)
+                self.blastList.append([x, y, 3, [[-1, 0], [1, 0], [0, 1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # lower edge
+            elif (y == self.size - 1 and x in range(1, self.size - 1) and self.cells[x][y][0] >= 3):
+                # self.animate_edge(tag, -1, 0, 1, 0, 0, -1)
+                self.blastList.append([x, y, 3, [[-1, 0], [1, 0], [0, -1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # middle
+            elif (x in range(1, self.size - 1) and y in range(1, self.size - 1) and self.cells[x][y][0] >= 4):
+                # self.animate_middle(tag, -1, 0, 1, 0, 0, -1, 0, 1)
+                self.blastList.append([x, y, 4, [[-1, 0], [1, 0], [0, -1], [0, 1]]])
+                self.play_sound("soundeffects/explode.wav")
+
+            # else:
+            #     print("Else condition")
+
+        self.animate()
+        self.adjustCell()
+        # print("before: ", len(self.cord_list2))
+        self.cord_list2 = self.cord_list2[1:]
+        # print("after: ", len(self.cord_list2))
